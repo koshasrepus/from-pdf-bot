a81badb5bd428684c9d60851d1cdb11e3812fb19
Александр Евсеев <alexander.evseev@balance-pl.ru>
2021-05-13 22:35:32 +0300

    edited settings and add test url

--- pdf_bot/settings.py
+++ pdf_bot/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2wqsg=*@i=9-1!$tfzxr!x6g-7%s$wh9(%_bth5_dj25=e*9gz'
+
+#SECRET_KEY = 'django-insecure-2wqsg=*@i=9-1!$tfzxr!x6g-7%s$wh9(%_bth5_dj25=e*9gz'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv('DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*",] # since Telegram uses a lot of IPs for webhooks
 
 
 # Application definition
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +127,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#-------> TELEGRAM
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
