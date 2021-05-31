61dd94369f5bd5b6d6236969a3a6458954a9bc45
Александр Евсеев <alexander.evseev@balance-pl.ru>
2021-05-31 23:07:25 +0300

    add migration

--- pdf_bot/settings.py
+++ pdf_bot/settings.py
@@ -25,7 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-#SECRET_KEY = 'django-insecure-2wqsg=*@i=9-1!$tfzxr!x6g-7%s$wh9(%_bth5_dj25=e*9gz'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
